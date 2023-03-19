a6e450a135b5cbe0b1bba20889aed8cb0be7fb9d
henior <henior@duck.com>
2023-03-19 10:47:43 +0100

    user ip test

--- sync-repo.php
+++ sync-repo.php
@@ -1,3 +1,22 @@
 <?php
 echo exec("cd /home/henior/domains/henior.kefir.ovh/public_html; git pull --rebase --autostash")
 ?>
+<?php
+
+function getUserIpAddr()
+{
+  if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+    //ip from share internet
+    $ip = $_SERVER['HTTP_CLIENT_IP'];
+  } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+    //ip pass from proxy
+    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+  } else {
+    $ip = $_SERVER['REMOTE_ADDR'];
+  }
+  return $ip;
+}
+
+echo 'User Real IP - ' . getUserIpAddr();
+
+?>
